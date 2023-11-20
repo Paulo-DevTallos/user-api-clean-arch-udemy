fb2723ddff6c8f6928dd34de0e7b2cce6ec78dae
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-11-20 18:07:16 -0300

    chore: set configs for jest

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,6 @@ export default {
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/',
   }),
-  rootDir: 'src',
   testRegex: '.*\\..*spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
