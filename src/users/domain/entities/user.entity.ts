4c6fb375976b8be9627bc305621a8d0ee0787181
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-11-21 19:49:26 -0300

    refactor: implement user validator

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,4 +1,5 @@
 import { Entity } from '@/shared/domain/entities/entity';
+import { UserValidatorFactory } from '../validators/user.validator';
 
 export type UserProps = {
   name: string;
@@ -12,15 +13,24 @@ export class UserEntity extends Entity<UserProps> {
     public readonly props: UserProps,
     id?: string,
   ) {
+    UserEntity.validateFields(props);
     super(props, id);
     this.props.created_at = this.props.created_at ?? new Date();
   }
 
+  static validateFields(props: UserProps) {
+    const validator = UserValidatorFactory.createValidation();
+    validator.validate(props);
+  }
+
   update(value: string) {
+    // valida tudo o que está nas props da instancia mais o valor que é repassado
+    UserEntity.validateFields({ ...this.props, name: value });
     this.name = value;
   }
 
   updatePassword(value: string) {
+    UserEntity.validateFields({ ...this.props, password: value });
     this.password = value;
   }
 
